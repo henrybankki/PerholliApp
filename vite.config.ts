d1cfdec36ccc9353844d49a196d5c6aba73abf6c
henrybankki <atsiu143@gmail.com>
2025-09-14 14:02:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './' // tärkee, jotta polut staattisiin resursseihin toimivat Renderissä
 })
